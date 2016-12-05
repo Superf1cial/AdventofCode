e7f5366a4da956367f730e1da98d57399045c1a8
Marvin Schröder <overwatchanalyst@gmail.com>
2016-12-05 10:12:09 +0100

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -1,14 +1,9 @@
 valid = 0
-invalid = 0
 
 with open("test.txt") as f:
     for line in f:
         test = line.split()
         test2 = ((list(map(int, test))))
-        if(test2[0] + test2[1] <= test2[2]):
-            invalid += 1
-        elif(test2[0] + test2[1] >= test2[2]):
+        if((test2[0] + test2[1] > test2[2]) and (test2[1] + test2[2] > test2[0]) and (test2[2] + test2[0] > test2[1])):
             valid += 1
-
-　
-print(valid, invalid)
+        print(valid)
